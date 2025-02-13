ab35a1a9373d99f58d5fb74495ba4d3a08da2405
Lisa HURTAUD <lisa.hurtaud@supdevinci-edu.fr>
2025-02-13 10:11:14 +0100

    Initial commit

---
+++ PawsitiveAPI/src/main/java/com/example/PawsitiveAPI/PawsitiveApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.PawsitiveAPI;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsitiveApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawsitiveApiApplication.class, args);
+	}
+
+}
---
+++ PawsitiveAPI/src/test/java/com/example/PawsitiveAPI/PawsitiveApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PawsitiveAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsitiveApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
