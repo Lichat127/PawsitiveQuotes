ecc82a7073f1dbcad6d71d95db11fafbd2421516
Lichat127 <65281248+Lichat127@users.noreply.github.com>
2025-02-13 14:10:33 +0100

    Initial commit

---
+++ PawsitiveChat/src/main/java/com/example/PawsitiveChat/PawsitiveChatApplication.java
@@ -0,0 +1,13 @@
+package com.example.PawsitiveChat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PawsitiveChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PawsitiveChatApplication.class, args);
+	}
+
+}
---
+++ PawsitiveChat/src/test/java/com/example/PawsitiveChat/PawsitiveChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.PawsitiveChat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PawsitiveChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
